c24bafb70bb5a39cfd7cdb2796703dde34719a20
Evgeniya <evgeniova@gmail.com>
2017-01-14 16:24:39 +0300

    first input added

--- app/controllers/CheckTypeController.js
+++ app/controllers/CheckTypeController.js
@@ -2,9 +2,13 @@ var app=angular.module('myApp');
 
 app.controller('checkTypeController', function ($scope) {
     var regArr=/\[.*\]/;
+    var regObj=/{.*}/;
     $scope.checkType= function () {
         if (regArr.test($scope.inputData) ) {
-            $scope.dataType="массив";
+            $scope.dataType= " Введенные данные представляют собой  массив";
+        }
+        else if (regArr.test($scope.inputData)) {
+            $scope.dataType= " Введенные данные представляют собой  объект";
         }
     }
 
---
+++ app/directives/checkTypeDirective.js
@@ -0,0 +1,8 @@
+var app=angular.module('myApp');
+
+app.directive ('checkTypeDirective', function () {
+    return {
+        templateUrl :'./templates/messageDataType.html'
+    }
+
+})
