b2fb44dd9f7c31c60f9cb5f89890db834b8c639a
Evgeniya <evgeniova@gmail.com>
2017-01-16 14:37:18 +0300

    function checktype ready

--- app/controllers/CheckTypeController.js
+++ app/controllers/CheckTypeController.js
@@ -3,12 +3,24 @@ var app=angular.module('myApp');
 app.controller('checkTypeController', function ($scope) {
     var regArr=/\[.*\]/;
     var regObj=/{.*}/;
+    var regFuncDecl=/function/;
+    var regFuncExpr=/function\(/;
     $scope.checkType= function () {
-        if (regArr.test($scope.inputData) ) {
+
+        if (regFuncExpr.test($scope.inputData)) {
+            $scope.dataType= " Введенные данные представляют собой  функцию вида Function Expression";
+        }
+        else if (regFuncDecl.test($scope.inputData)) {
+            $scope.dataType= " Введенные данные представляют собой  функцию вида Function Declaration";
+        }
+        else if (regArr.test($scope.inputData) ) {
             $scope.dataType= " Введенные данные представляют собой  массив";
         }
-        else if (regArr.test($scope.inputData)) {
+        else if (regObj.test($scope.inputData)) {
             $scope.dataType= " Введенные данные представляют собой  объект";
+        }
+        else {
+            $scope.dataType= "Данные не распознаны";
         }
     }
 
